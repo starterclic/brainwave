693672907fc78ac5b4f893a03bb615198567b3eb
François <66567889+starterclic@users.noreply.github.com>
2024-08-02 12:07:47 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,8 +36,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+            Vous êtes submergé par la vague IA ? Noyé dans l'océan des possibilités technologiques ? Cybtek est votre bouée de sauvetage !
           </p>
           <Button href="/pricing" white>
             Get started
