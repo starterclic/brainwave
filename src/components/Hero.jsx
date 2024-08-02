91789fff01e4f573d82434b79c430ec28afccf5f
François <66567889+starterclic@users.noreply.github.com>
2024-08-02 12:06:01 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,9 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            Votre Antidote à la Paralysie Numérique: {` `}
             <span className="inline-block relative">
-              Brainwave{" "}
+              Cybtek{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
